30031d26f0858c6e9d7bd7429e225dab7a393a5f
Nasser Khawja <nasser.khawja@gmail.com>
2024-09-19 14:00:54 -0700

    Create Inst-launch.sh

---
+++ Inst-launch.sh
@@ -0,0 +1,10 @@
+aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --count 1 \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --security-group-ids $SG_ID \
+  --subnet-id $SUBNET_ID \
+  --associate-public-ip-address \
+  --user-data file://kali_xrdp_userdata.sh \
+  --query 'Instances[0].InstanceId' --output text
